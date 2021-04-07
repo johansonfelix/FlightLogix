c59d8a77e8cce5fd71250578c0dcc0e1fdbd5c46
Johanson Felix <felix.johanson@gmail.com>
2021-04-07 17:53:19 -0400

    AmadadeusAPICaller changes parseItinerary

---
+++ FlightLogix/src/main/java/com/FlightLogix/Core/Booking/FlightData.java
@@ -0,0 +1,33 @@
+package com.FlightLogix.Core.Booking;
+
+import com.FlightLogix.Core.Flight.Flight;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FlightData {
+    private ArrayList<Flight> flights;
+    private HashMap<String,String> carrierNames; // Carrier Code to Carrier name.
+
+    public FlightData(){
+
+    }
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(ArrayList<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public HashMap<String,String> getCarrierNames() {
+        return carrierNames;
+    }
+
+    public void setCarrierNames(HashMap<String,String> carrierNames) {
+        this.carrierNames = carrierNames;
+    }
+
+
+
+}
