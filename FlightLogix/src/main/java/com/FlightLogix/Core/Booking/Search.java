c486e6e66d09eb61ddca25539488f2cc0893ae88
Johanson Felix <felix.johanson@gmail.com>
2021-04-07 00:16:36 -0400

    Search Class added

---
+++ FlightLogix/src/main/java/com/FlightLogix/Core/Booking/Search.java
@@ -0,0 +1,68 @@
+package com.FlightLogix.Core.Booking;
+
+public class Search {
+    private String originLocationCode;
+    private String destinationLocationCode;
+    private String departureDate;
+    private String returnDate;
+    private int numAdults;
+    private int maxResults;
+
+    public Search(String originLocationCode, String destinationLocationCode, String departureDate, String returnDate, int numAdults, int maxResults) {
+        this.originLocationCode = originLocationCode;
+        this.destinationLocationCode = destinationLocationCode;
+        this.departureDate = departureDate;
+        this.returnDate = returnDate;
+        this.numAdults = numAdults;
+        this.maxResults = maxResults;
+    }
+
+    public Search(){}
+    public String getOriginLocationCode() {
+        return originLocationCode;
+    }
+
+    public void setOriginLocationCode(String originLocationCode) {
+        this.originLocationCode = originLocationCode;
+    }
+
+    public String getDestinationLocationCode() {
+        return destinationLocationCode;
+    }
+
+    public void setDestinationLocationCode(String destinationLocationCode) {
+        this.destinationLocationCode = destinationLocationCode;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public int getNumAdults() {
+        return numAdults;
+    }
+
+    public void setNumAdults(int numAdults) {
+        this.numAdults = numAdults;
+    }
+
+    public int getMaxResults() {
+        return maxResults;
+    }
+
+    public void setMaxResults(int maxResults) {
+        this.maxResults = maxResults;
+    }
+}
