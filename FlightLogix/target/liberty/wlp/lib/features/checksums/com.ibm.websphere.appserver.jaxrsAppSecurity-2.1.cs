a1fa9e22b9844809776eda4c3bd4cd6af9689284
Angoram <kor.andrew@outlook.com>
2021-04-07 00:39:09 -0400

    jsonarray changes.

---
+++ FlightLogix/target/liberty/wlp/lib/features/checksums/com.ibm.websphere.appserver.jaxrsAppSecurity-2.1.cs
@@ -0,0 +1,3 @@
+#Wed Apr 07 00:20:36 EDT 2021
+lib/com.ibm.ws.jaxrs.2.0.security_1.0.50.jar=7ca8b266ad3c4f298c8c551b07c4fcca
+lib/features/com.ibm.websphere.appserver.jaxrsAppSecurity-2.1.mf=1e1967287aff9fd440a2d8eb539848e9
---
+++ FlightLogix/target/liberty/wlp/lib/features/checksums/com.ibm.websphere.appserver.transportSecurity-1.0.cs
@@ -0,0 +1,2 @@
+#Wed Apr 07 00:20:31 EDT 2021
+lib/features/com.ibm.websphere.appserver.transportSecurity-1.0.mf=708489c94e76af4d1c35a8cd08fd9eea
