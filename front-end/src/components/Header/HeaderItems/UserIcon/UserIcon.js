32bc9738f78edf9239481a9ac61ea4dfdf140da3
Johanson Felix <felix.johanson@gmail.com>
2021-04-13 07:13:24 -0400

    FrontEnd

--- front-end/src/components/Header/HeaderItems/UserIcon/UserIcon.js
+++ front-end/src/components/Header/HeaderItems/UserIcon/UserIcon.js
@@ -1,11 +1,12 @@
 import classes from './UserIcon.module.css';
-import usericon from '../../../../assets/user.png';
+import {Icon} from '@iconify/react';
+import userCircle from '@iconify-icons/fa/user-circle';
 
 const UserIcon = () => {
     return(
 
         <div className={classes.icon}>
-            <img src={usericon} alt="user" />
+            <Icon className={classes.icon}icon={userCircle} />
         </div>
 
     );
