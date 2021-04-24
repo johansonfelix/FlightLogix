14fbb3693d910d693bbae3c2b97da692cada221b
Johanson Felix <felix.johanson@gmail.com>
2021-04-24 17:19:00 -0400

    updates

--- front-end/src/Pages/AdminDashboard/Dashboard.js
+++ front-end/src/Pages/AdminDashboard/Dashboard.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
@@ -13,7 +12,6 @@ import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import FlightSearchCard from '../../Components/FlightSearchCard';
@@ -62,19 +60,6 @@ ElevationScroll.propTypes = {
      */
     window: PropTypes.func,
 };
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-      </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
 
 
 
@@ -367,10 +352,7 @@ export default function Dashboard(props) {
               </Paper>
             </Grid>
           </Grid>
-          <Box pt={4}>
-            <Copyright />
-          </Box>
-       
+                 
         </Route>
 
         <Route path="/viewusers">
